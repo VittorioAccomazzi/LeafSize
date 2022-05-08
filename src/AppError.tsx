e9375cfebf93b1329e9736e27f84053c33f40e6f
Vittorio Accomazzi <Vittorio.Accomazzi@gmail.com>
2022-05-07 21:44:59 -0400

    re #8 improved error reporting

--- src/AppError.tsx
+++ src/AppError.tsx
@@ -6,17 +6,17 @@ import {FallbackProps} from 'react-error-boundary'
 export default function AppError( prop:FallbackProps) {
 
     const error = prop.error.message;
-    const stack = prop.error.stack ?? "Stack not available";
+    const stack = prop.error.stack ?  prop.error.stack: "Stack not available";
     return(
         <Box>
             <Typography variant="h5" component="div">
                     ❌ An unexpected error occurred : <span style={{color:'blue'}}> <b><code>{error}</code></b></span> <br/>
                     Please  &nbsp;                 
                     <Link underline='hover' color="textSecondary"
-                        href= { "mailto:vittorio.accomazzi+LeafSize@gmail.com?subject=Report report on version"+gitVersion.long
-                                 + "&body=Encounterd Error "+error+".%0D%0A%0D%0A Stack: %0D%0A"
-                                 + stack  + 
-                                 + "%0D%0A% Please describe the step to reproduce the error"} 
+                        href= { "mailto:vittorio.accomazzi+LeafSize@gmail.com?subject=Report report on version "+gitVersion.long
+                                 + "&body=Encounterd Error "+error+".%0D%0A%0D%0AStack: %0D%0A"
+                                 + stack.replaceAll("\n","%0D%0A ") + 
+                                 + "%0D%0A Please describe the step to reproduce the error"} 
                                  target="_blank">
                         report the error
                     </Link>
